9a7667dd59446cd8f88b69b61501c7fc3a257006
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-28 15:46:34 +0200

    Initial Commit

---
+++ CustomContainers/CustomContainersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomContainersApp.swift
+//  CustomContainers
+//
+//  Created by Vahtee Boo on 28.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomContainersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
